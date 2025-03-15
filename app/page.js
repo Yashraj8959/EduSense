df1922f700feb04d88be8739a7dfd8b7f550e238
Yashraj8959 <ykushwah7828@gmail.com>
2025-03-16 03:19:36 +0530

    Add HeroSection component with scroll effect on banner

--- app/page.js
+++ app/page.js
@@ -1,18 +1,12 @@
+"use client"
 import { Button } from "@/components/ui/button"
-
-
+import HeroSection from "@/components/Hero";
 export default function Home() {
   return (
     <>
     <div className="w-full grid-background"></div>
-    <div className="container mx-auto px-4 py-12 text-center">
-      <h1>Welcome to EduSense</h1>
-      <p>
-        A career growth app
-      </p>
-      <Button>Get Started</Button>
-
-    </div>
+    {/* HeroSection */}
+    <HeroSection/>
     
 
     </>
