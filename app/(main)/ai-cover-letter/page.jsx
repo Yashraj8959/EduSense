0b7c37117d4c118eb7e65f7f391af0f7501662b8
Yashraj8959 <ykushwah7828@gmail.com>
2025-03-17 04:09:16 +0530

    Add new Routes by adding folders and files for cover letter, dashboard, interview, onboarding, and resume sections

---
+++ app/(main)/ai-cover-letter/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const coverLetterPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default coverLetterPage
---
+++ app/(main)/interview/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const mainlayout = ({children}) => {
+  return (
+    {children}
+  )
+}
+
+export default mainlayout
